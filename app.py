a16263fafba659512d0d5603bb2077bb032a7f0f
consti <pnpnsx@gmail.com>
2026-07-19 02:11:06 +0300

    Update server port to 8000
    Change the server port from 5050 to 8000.

--- app.py
+++ app.py
@@ -438,4 +438,4 @@ async def websocket_endpoint(websocket: WebSocket, board_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5050)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
